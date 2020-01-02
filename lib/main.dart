eeedd68c8eb89f96d39148933d778723984f7f9e
rlf3 <rlf3@hotmail.com>
2020-01-02 18:14:05 -0500

    update

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:custom_splash/custom_splash.dart';
+
+
+import 'package:imap/utils/myColors.dart';
+import 'package:imap/widgets/header.dart';
+import 'package:imap/widgets/body.dart';
+
+void main(){
+
+  Function duringSplash = () {
+    print('Something background process');
+    int a = 123 + 23;
+    print(a);
+
+    if (a > 100)
+      return 1;
+    else
+      return 2;
+  };
+
+  
+
+
+
+  //debugPaintSizeEnabled = true;
+  //runApp(new MyApp());
+
+  runApp(MaterialApp(
+
+  
+  initialRoute: '/',
+/*     routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      //'/': (context) => MyApp(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      //'/route': (context) => RootPage(auth: new Auth()),
+      //'/route': (context) => RootPage(auth: new Auth()),
+      '/login': (context) => LoginSignUpPage(auth: new Auth()),
+      loginRoute: (context) => LoginSignUpPage(auth: new Auth()),
+    }, */
+  //onGenerateRoute: RouteGenerator.generateRoute,
+
+
+
+    debugShowCheckedModeBanner: false,
+    home: CustomSplash(
+      
+      imagePath: 'assets/app-icon.png',
+      backGroundColor: Colors.white,
+      // backGroundColor: Color(0xfffc6042),
+      animationEffect: 'zoom-in',
+      logoSize: 200,
+      home: MyApp(),
+      customFunction: duringSplash,
+      duration: 2500,
+      type: CustomSplashType.StaticDuration,
+      //outputAndHome: op,
+    ),
+  ));
+
+  
+
+} 
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IMap !',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Add a container and provide a linear gradient.
+    //basically use different shades of same color.
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [MyColors.white1, MyColors.white2])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[Header(), Body()],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  static const String appTitle = "Maps to think !";
+  static const String logoTitle = "IMap";
+  static const String loginButton = "Login";
+  static const String hello = "Wellcome!";
+  static const String welcomeTo = "Let's ";
+  static const String ftf= "Mapping to think together !";
+  static const String subscribeText = "Subscribe to keep informed";
+  static const String email_hint = "Email address please";
+  static const String subscribeButton = "Subscribe";
+}
